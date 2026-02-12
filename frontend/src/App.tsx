b93c78e9fb866ba67cc44cdd5c3f3a40f4be89d3
Joaokenehen <joaogustavokenehen@gmail.com>
2026-02-12 20:50:17 -0300

    refactor: create navigation on app and done folder organization

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Servicos from './screens/servicos'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Futura página de Portfólio (Home) */}
+        <Route path="/portifolio" element={<div>Sua futura Home/Portfólio aqui</div>} />
+
+        {/* Sua página atual de serviços da JGTech */}
+        <Route path="/servicos" element={<Servicos />} />
+
+        {/* REDIRECIONAMENTO: Qualquer rota não encontrada ou ao entrar no site,
+            ele manda direto para /servicos por enquanto */}
+        <Route path="*" element={<Navigate to="/servicos" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
