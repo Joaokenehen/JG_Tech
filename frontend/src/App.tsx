f4fc5e0c3bfa448e1f4ae970e718811c87e00ac5
Joaokenehen <joaogustavokenehen@gmail.com>
2026-02-12 20:53:41 -0300

    refactor: archive bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Servicos from './screens/servicos'
+import Servicos from './screens/Servicos'
 
 function App() {
   return (
