4dfd3ebff6936ad6cdf93e2bcf93f4f0e3831ff3
Joaokenehen <joaogustavokenehen@gmail.com>
2026-02-13 08:53:04 -0300

    refactor: adicionado react-router-dom para rotas corretas de servicos e futuro portifolio

---
+++ frontend/src/screens/Portifolio.tsx
@@ -0,0 +1,7 @@
+export default function Portifolio() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-2xl text-slate-400">
+      Seu futuro Portfólio aqui
+    </div>
+  );
+}
