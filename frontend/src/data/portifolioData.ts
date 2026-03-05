d998d2557396ff027dd2198e94bca063e62899c5
Joaokenehen <joaogustavokenehen@gmail.com>
2026-03-05 14:28:21 -0300

    feat: adicionado techs

--- frontend/src/data/portifolioData.ts
+++ frontend/src/data/portifolioData.ts
@@ -1,5 +1,5 @@
 import { FaReact, FaNodeJs, FaJs, FaGithub, FaLinkedin, FaWhatsapp, FaJava } from 'react-icons/fa';
-import { SiTypescript, SiVite, SiC, SiMysql, SiSequelize, SiGit, SiNpm, SiDocker, SiPostgresql, SiTailwindcss, SiHtml5, SiCss3, SiSpringboot, SiSwagger, SiChocolatey, SiRedis, SiExpo, SiVercel, SiGithub, SiPostman } from 'react-icons/si';
+import { SiTypescript, SiVite, SiC, SiMysql, SiSequelize, SiGit, SiNpm, SiDocker, SiPostgresql, SiTailwindcss, SiHtml5, SiCss3, SiSpringboot, SiSwagger, SiChocolatey, SiRedis, SiExpo, SiVercel, SiGithub, SiPostman, SiNginx, SiExpress } from 'react-icons/si';
 
 export const techStack = [
   { name: 'React', icon: FaReact, color: '#61DAFB', shadow: 'shadow-blue-500/20' },
@@ -26,6 +26,8 @@ export const techStack = [
   { name: 'Vercel', icon: SiVercel, color: '#ece3e3', shadow: 'shadow-black/20' },
   { name: 'GitHub', icon: SiGithub, color: '#f5eded', shadow: 'shadow-gray-800/20' },  
   { name: 'Postman', icon: SiPostman, color: '#FF6C37', shadow: 'shadow-orange-500/20' },
+  { name: 'Nginx', icon: SiNginx, color: '#009639', shadow: 'shadow-green-500/20' },
+  { name: 'Express', icon: SiExpress, color: '#000000', shadow: 'shadow-black/20' },
 ];
 
 export const socialLinks = [
